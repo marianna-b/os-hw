f8096b1d24f391dfe05baf42083ac9da43709ad1
mariashka <bisyarinamariashka@gmail.com>
2015-03-08 18:24:49 +0300

    Fix bug with including library

--- cat/cat.c
+++ cat/cat.c
@@ -1,5 +1,7 @@
-#include "helpers.h"
-#include "string.h"
+#include "../lib/helpers.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
 int main() {
 	char buf[1024];
@@ -7,13 +9,18 @@ int main() {
 	while ((read_res = read_(STDIN_FILENO, buf, 1024)) > 0) {
 		if ((write_res = write_(STDOUT_FILENO, buf, read_res)) < 0) {
 			perror("Can't write to stdout");
-			goto ERROR
+			goto ERROR;
 		}
+		
+		if (read_res < 1024)
+			break;
 	}
+	
 	if (read_res < 0) {
 		perror("Can't read from stdin");
-		goto ERROR
+		goto ERROR;
 	}
+	
 	return 0;
 
 ERROR:
