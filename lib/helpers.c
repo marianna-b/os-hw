9b2df60cacd3c5ae1d218ac4f3f332b2764bb952
mariashka <bisyarinamariashka@gmail.com>
2015-03-08 14:43:59 +0300

    Add function definitions

--- lib/helpers.c
+++ lib/helpers.c
@@ -1,1 +1,9 @@
 #include "helpers.h"
+
+static ssize_t write_(int fd, const void* buf, size_t count) {
+	return 0;
+}
+
+static ssize_t read_(int fd, const void* buf, size_t count) {
+	return 0;
+}
--- lib/helpers.h
+++ lib/helpers.h
@@ -1,8 +1,9 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
-struct helpers {
-	
-};
+#include <sys/types.h>
+
+static ssize_t write_(int, const void*, size_t);
+static ssize_t read_(int, const void*, size_t);
 
 #endif
