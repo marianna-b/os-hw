3f23d1b9d6b01f1cadc2c62faf58fcbdc92091b0
mariashka <bisyarinamariashka@gmail.com>
2015-03-22 23:31:26 +0300

    Fix bug with spawn return value

--- lib/helpers.c
+++ lib/helpers.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 ssize_t write_(int fd, const void* buf, size_t count) {
 	size_t idx = 0;
@@ -58,13 +59,11 @@ int spawn(const char* file, char* const argv[]) {
 	pid_t child;
 	
 	if ((child = fork()) == 0) {
-		return execvp(file, argv);
+		execvp(file, argv);
+		_exit(0);
 	} else {
-		int status = 1;
-		int ret = waitpid(child, &status, 0);
-		if (ret < 0 || WIFEXITED(status))
-			return 0;
-		else
-			return -1;
+		int status;
+		waitpid(child, &status, 0);
+		return status;
 	}
 }
