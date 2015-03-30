1172e0797fbdfcb27190b592ef0ccaf00aa74d48
mariashka <bisyarinamariashka@gmail.com>
2015-03-30 19:50:09 +0300

    Fix bug with failing to create buffer

--- bufcat/bufcat.c
+++ bufcat/bufcat.c
@@ -7,6 +7,10 @@ const size_t BUF_SIZE = 4096;
 
 int main() {
 	buf_t* buf = buf_new(BUF_SIZE);
+	if (buf == NULL) {
+		perror("Error creating buffer");
+		goto ERROR;
+	}
 	while (1) {
 		ssize_t read_res = buf_fill(STDIN_FILENO, buf, BUF_SIZE);
 		if (buf_flush(STDOUT_FILENO, buf, BUF_SIZE) < 0) goto ERROR;
