0b16eb0325c449e869dc3d19cfc3d5ac266e3852
mariashka <bisyarinamariashka@gmail.com>
2015-05-26 18:36:15 +0300

    Add somrthing about runpiped

--- lib/helpers.c
+++ lib/helpers.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <signal.h>
 
 ssize_t write_(int fd, const void* buf, size_t count) {
 	size_t idx = 0;
@@ -69,4 +70,135 @@ int spawn(const char* file, char* const argv[]) {
 		else
 			return 0;
 	}
+}
+
+const int ARGSIZE = 1000;
+
+struct execargs_t {
+	char** arg;
+	size_t size;
+};
+
+
+execargs_t* execargs_new(char* str, size_t size) {
+	execargs_t* p = malloc(sizeof(execargs_t));
+	if (p == NULL) {
+		return NULL;
+	}
+	size_t i, l = 0;
+	int amount = 0;
+	for (i = 0; i< size; i++){
+		if ((str[i] == ' ') && (i > l)) {
+			l = i + 1;
+			amount++;
+		}
+	}
+	if (l < size)
+		amount++;
+	amount++;
+	
+	p->size = amount;
+	p->arg = calloc(amount, sizeof(char*));
+	if (p->arg == NULL) {
+		free(p);
+		return NULL;
+	}
+	l = 0, amount = 0;
+	for (i = 0; i< size; i++){
+		if ((str[i] == ' ') && (i > l)) {
+			l = i + 1;
+			int len = i + 1 - l;
+			p->arg[amount] = calloc(len, sizeof(char));
+			if (p->arg[amount] == NULL) {
+				int j;
+				for (j = 0; j < amount; j++)
+					free(p->arg[j]);
+				free(p->arg);
+				free(p);
+				return NULL;
+			}
+			memcpy(p->arg[amount], str + l, len - 1);
+			amount++;
+		}
+	}
+	int len = size + 1 - l;
+	if (l != size) {
+		p->arg[amount] = calloc(len, sizeof(char));
+		if (p->arg[amount] == NULL) {
+			int j;
+			for (j = 0; j < amount; j++)
+				free(p->arg[j]);
+			free(p->arg);
+			free(p);
+			return NULL;
+		}
+		memcpy(p->arg[amount], str + l, len - 1);
+	}
+	return p;
+}
+
+void execargs_free(execargs_t* p) {
+	size_t j;
+	for (j = 0; j < p->size; j++)
+		free(p->arg[j]);
+	free(p->arg);
+	free(p);
+}
+
+int exec(execargs_t* args) {
+	execvp(args->arg[0], args->arg);
+	_exit(EXIT_FAILURE);
+}
+
+void handler(int sig) {
+	signal(SIGQUIT, SIG_IGN);
+	kill(-1 * getpid(), SIGINT);
+}
+
+void handler_set() {
+	struct sigaction act;
+	memset(&act, 0, sizeof(act));
+	act.sa_handler = handler;
+	sigset_t set;
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	act.sa_mask = set;
+	sigaction(SIGINT, &act, 0);
+}
+
+int runpiped(execargs_t** programs, size_t n) {
+	pid_t children[n];
+	size_t i;
+	int currstdin = STDIN_FILENO;
+	for (i = 0; i < n; i++) {
+		
+		int currstdout;
+		int pipefd[2];
+		if (i == n -1) {
+			currstdout = STDOUT_FILENO;
+		} else {
+			if (pipe(pipefd) < 0) {
+				return -1;
+			}
+			currstdout = pipefd[1];
+		}
+
+		if ((children[i] = fork()) == 0) {
+			if (dup2(currstdin, STDIN_FILENO) < 0) return -1;
+			if (dup2(currstdout, STDOUT_FILENO) < 0) return -1;
+			if (exec(programs[i]) != 0)  return -1;
+		} else {
+			if (children[i] < 0) {
+				return -1;
+			}
+			currstdin = pipefd[0];
+		}
+		int status;
+		waitpid(children[n - 1], &status, 0);
+		if (WEXITSTATUS(status) != 0)
+			return -1;
+		return 0;
+	}
+	
+	return 0;
 }
--- lib/helpers.h
+++ lib/helpers.h
@@ -3,8 +3,18 @@
 
 #include <sys/types.h>
 
+struct execargs_t;
+typedef struct execargs_t execargs_t;
+
 ssize_t write_(int, const void*, size_t);
 ssize_t read_(int, const void*, size_t);
 ssize_t read_until(int, void *, size_t, char);
 int spawn(const char*, char* const[]);
+
+execargs_t* execargs_new(char*, size_t);
+void execargs_free(execargs_t*);
+
+int exec(execargs_t * args);
+int runpiped(execargs_t**, size_t);
+
 #endif
