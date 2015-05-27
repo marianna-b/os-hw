991a8b058060af3d9e320d57e0647572292e9ad1
mariashka <bisyarinamariashka@gmail.com>
2015-05-27 23:18:18 +0300

    Add handling interrupted system calls and fix bugs

--- simplesh/simplesh.c
+++ simplesh/simplesh.c
@@ -1,21 +1,31 @@
-#include "helpers.h"
+#include <helpers.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <signal.h>
 
 const size_t BUF_SIZE = 4096;
 const char delimiter = '\n';
 const char pipechar = '|';
-const char* str = "$ ";
+const char* str = "$";
 
 int main() {
+	signal(SIGINT, SIG_IGN);
 	char buf[BUF_SIZE];
+
 	execargs_t* progs[BUF_SIZE];
-	if (write_(STDIN_FILENO, str, 2) < 0) goto ERROR;
-	int amount = 0, idx = 0, readres;
+	if (write_(STDIN_FILENO, str, 1) < 0) goto ERROR;
 	
-	while ((readres = read_until(STDIN_FILENO, buf + idx, BUF_SIZE - idx, delimiter)) > 0) {
+	int amount = 0, idx = 0, readres;
+	while ((readres = read_until(STDIN_FILENO, buf + idx, BUF_SIZE - idx, delimiter)) != 0) {
+		if (readres < 0) {
+			if (errno == EINTR) 
+				continue;
+			else 
+				break;
+		}
 		size_t p = 0, n = readres + idx;
 		int i;
 		for (i = 0; i < (int)n; i++) {
@@ -27,20 +37,28 @@ int main() {
 				p = i + 1;
 				if (buf[i] == delimiter) {
 					if (runpiped(progs, amount) < 0) goto ERROR;
+
+					int j;
+					for (j = 0; j < amount; j++) {
+						execargs_free(progs[j]);
+					}
+
 					amount = 0;
-					if (write_(STDIN_FILENO, str, 2) < 0) goto ERROR;
+					if (write_(STDIN_FILENO, str, 1) < 0) goto ERROR;
 				}
 			}
 		}
 		memmove(buf, buf + p, readres - p);
 		idx = readres - p;
 	}
-
-	if (readres < 0) {
-		perror("Can't read from stdin");
-		goto ERROR;
+	int j;
+	for (j = 0; j < amount; j++) {
+		execargs_free(progs[j]);
 	}
+	if (readres < 0) goto ERROR;
+	
 	return 0;
 ERROR:
+	perror("");
 	return 1;
 }
